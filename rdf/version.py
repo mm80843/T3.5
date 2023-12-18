84ff01397b4e9fe56405f41e67d48955ebaca555
mm80843 <luc.jonveaux@mottmac.com>
2023-12-18 14:45:12 +0100

    Restructure of repo.

---
+++ rdf/version.py
@@ -0,0 +1,4 @@
+DATE            = 'Creation: 11/11/2023'
+VERSION         = 'Version: 0.3'
+VERSION_COMMENT = 'VersionComment: New blueprints added in 0.3'
+OWLREADY2       = 'Library: owlready2==0.45'
