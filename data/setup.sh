46708dee0b0e1c756f0d25c57f02f391710f9821
kelu124 <kelu124@gmail.com>
2024-01-22 23:06:53 +0100

    tst

--- data/setup.sh
+++ data/setup.sh
@@ -1,3 +1,3 @@
-cp ../../rdf/pbn_t3_5.owl.zip ./onto.zip
+cp ../../rdf/work/work_on_0.3/WIP.owl ./onto.zip
 cp ../../rdf/support/order.json ./order.json
 cp ../../utils/kgartefacts.py ../kgartefacts.py
