750658e79f6711eaa68154c33fe67633a9cec236
kelu124 <kelu124@gmail.com>
2024-03-28 22:08:07 +0100

    Update

--- data/setup.sh
+++ data/setup.sh
@@ -1,7 +1,7 @@
 cp ../../rdf/prod/pbn_t3_5_simple.owl ./onto.owl
 
-cp ../../rdf/work/work_on_0.4/WIP_1.owl ./onto.owl
-cp ../../rdf/work/work_on_0.4/WIP_1.owl ./full.owl
+cp ../../rdf/work/work_on_0.4/WIP_2.owl ./onto.owl
+cp ../../rdf/work/work_on_0.4/WIP_2.owl ./full.owl
 
 cp ../../rdf/support/order.json ./order.json
 cp ../../utils/kgartefacts.py ../kgartefacts.py
